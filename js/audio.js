23f7f2a4bc26b658842bf65740b81bbc9b437638
aeonicentity <aeonicentity@gmail.com>
2016-04-24 09:24:02 -0600

    removing bad files

--- js/audio.js
+++
@@ -1,5 +0,0 @@
-var deathSound = new Audio("assets/death.wav");
-var explodeSound = new Audio("assets/explode.wav");
-var fireSound = new Audio("assets/fire.wav");
-var towerplaceSound = new Audio("assets/towerplace.wav");
-var towersellSound = new Audio("assets/towersell.wav");
